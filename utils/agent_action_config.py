ec02da999a6cd4840e84a3af4dbb37a8bafd4f4e
CICarlier <35584629+CICarlier@users.noreply.github.com>
2022-01-09 20:41:39 -0500

    updated agent_action_config.py to latest version of toolkit ut preserved map_action_policy function

--- utils/agent_action_config.py
+++ utils/agent_action_config.py
@@ -6,34 +6,56 @@ _agent_action_config_action_size = 5
 
 
 def get_flatland_full_action_size():
-    # The action space of flatland is 5 discrete actions
+    '''
+    Just get the default RailEnvActions space size back
+    :return: The action space of flatland is 5 discrete actions
+    '''
     return 5
 
 
 def set_action_size_full():
+    '''
+    Set a global variable to define the action space size to full
+    If set -> the build-in RailEnvActions will be used (default)
+    '''
     global _agent_action_config_action_size
     # The agents (DDDQN, PPO, ... ) have this actions space
     _agent_action_config_action_size = 5
 
 
 def set_action_size_reduced():
+    '''
+    Set a global variable to define the action space size to reduced
+    If set -> the actions space has the DO_NOTHING action removed from RailEnvActions
+    :return:
+    '''
     global _agent_action_config_action_size
     # The agents (DDDQN, PPO, ... ) have this actions space
     _agent_action_config_action_size = 4
 
 
 def get_action_size():
+    '''
+    :return: The current active action space size
+    '''
     global _agent_action_config_action_size
     # The agents (DDDQN, PPO, ... ) have this actions space
     return _agent_action_config_action_size
 
 
 def map_actions(actions):
+    '''
+    Converts the actions in the actions array. Iff the actions space is not set to full.
+    :param actions: Array of actions to convert
+    :return: converted actions as array
+    '''
     # Map the
     if get_action_size() != get_flatland_full_action_size():
+        ret_actions = {}
         for key in actions:
             value = actions.get(key, 0)
-            actions.update({key: map_action(value)})
+            ret_actions.update({key: map_action(value)})
+        return ret_actions
     return actions
 
 
@@ -44,6 +66,12 @@ def map_action_policy(action):
 
 
 def map_action(action):
+    '''
+    if the action space is full -> no tranformation will be done (just using the RailEnvActions). Otherwise the action
+    will be transformed/encoded -> Action [0,1,2,3] -> RailEnvActions -> DO_NOTHING does no longer exist.
+    :param action: action to convert/transform : number in [0,1,2,3]
+    :return: transformed action
+    '''
     if get_action_size() == get_flatland_full_action_size():
         return action
 
@@ -55,9 +83,15 @@ def map_action(action):
         return RailEnvActions.MOVE_RIGHT
     if action == 3:
         return RailEnvActions.STOP_MOVING
+    return RailEnvActions.STOP_MOVING
 
 
-def map_rail_env_action(action):
+def convert_default_rail_env_action(action):
+    '''
+    Converts a RailEnvActions (0,1,2,3,4)
+    :param action: RailEnvAction or a number in [0,1,2,3,4]
+    :return: Iff action space is set to full, no transformation will be done otherwise replace action DO_NOTHING -> STOP
+    '''
     if get_action_size() == get_flatland_full_action_size():
         return action
 
